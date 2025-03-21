938cc5c4a0915995b489fc6955339890be056725
Bruce Howard <61521590+brucehoward-physics@users.noreply.github.com>
2025-03-20 23:08:55 -0400

    Fix ordering

--- src/Objects/CaloHit.cc
+++ src/Objects/CaloHit.cc
@@ -75,9 +75,9 @@ CaloHit::CaloHit(const object_creation::CaloHit::Parameters &parameters) :
     m_isIsolated(false),
     m_isAvailable(true),
     m_weight(1.f),
-    m_pParentAddress(parameters.m_pParentAddress.Get()),
     m_mcMatchWeight(0.),
-    m_mcMatchPDG(0)
+    m_mcMatchPDG(0),
+    m_pParentAddress(parameters.m_pParentAddress.Get())
 {
     m_cellLengthScale = this->CalculateCellLengthScale();
 }
